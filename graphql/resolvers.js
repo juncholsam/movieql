1a898fc2ac780e592e39821c145b6f9a22e540a9
Lee JunChol <juncholsam@megastudy.net>
2020-02-12 17:12:31 +0900

    final basics

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,46 +1,8 @@
-const people = [
-    {
-        id: "1",
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
-    },
-    {
-        id: "2",
-        name: "JUN",
-        age: 25,
-        gender: "male"
-    },
-    {
-        id: "3",
-        name: "SO",
-        age: 11,
-        gender: "female"
-    },
-    {
-        id: "4",
-        name: "TWO",
-        age: 67,
-        gender: "male"
-    },
-    {
-        id: "5",
-        name: "Malia",
-        age: 55,
-        gender: "female"
-    },
-    {
-        id: "6",
-        name: "Tendong",
-        age: 90,
-        gender: "male"
-    }
-];
-
+import { getMovies } from "./db"
 
 const resolvers = {
     Query: {
-        people: () => people,
+        movies: (_, { rating, limit }) => getMovies(limit, rating)
     }
 };
 
